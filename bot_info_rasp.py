a97cd9c1713146cee352d49cb54ed79fe8bfc1f5
Abdias Viana <abdviana@gmail.com>
2019-05-27 12:11:51 -0300

    ...

--- bot_info_rasp.py
+++ bot_info_rasp.py
@@ -10,19 +10,29 @@ oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service'
 botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
 old_ip = get_ip()
 
+if old_ip != get_ip():
+        send_info(message)
+
 @bot.message_handler(commands = ['info', 'start'])
 def send_info(message):
-    old_ip = get_ip()
-    now = get_time()
-    bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
-    bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
-    bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
-    bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
-    bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
+        old_ip = get_ip()
+        now = get_time()
+        bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
+        bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
+        bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
+        bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
+        bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
 
-    bot.send_message(message.chat.id, '-----------------------------------------------')
-    bot.send_message(message.chat.id, oCatolicoBotService)
-    bot.send_message(message.chat.id, '-----------------------------------------------')
-    bot.send_message(message.chat.id, botInfoRpi)  
+        oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service')
+        botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
+        bot.send_message(message.chat.id, '-----------------------------------------------')
+        bot.send_message(message.chat.id, oCatolicoBotService)
+        bot.send_message(message.chat.id, '-----------------------------------------------')
+        bot.send_message(message.chat.id, botInfoRpi)  
+
+@bot.message_handler(commands = [reboot])
+def send_reboot_command(message):
+        bot.send_message(message.chat.id, 'Raspberry Reiniciado')
+        reboot_rasp()
 
 bot.polling()
