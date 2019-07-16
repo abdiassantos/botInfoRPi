f77fe7852de2e9b8adbb928e5ff8b45fd03de790
abdiasviana <abdviana@gmail.com>
2019-07-16 13:21:33 -0300

    ahsbhdgasd

--- bot_info_rasp.py
+++ bot_info_rasp.py
@@ -11,21 +11,20 @@ botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
 
 @bot.message_handler(commands = ['info', 'start'])
 def send_info(message):
-        if(message.chat.id == '48443118'):
-                old_ip = get_ip()
-                now = get_time()
-                bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
-                bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
-                bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
-                bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
-                bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
-
-                oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service')
-                botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
-                bot.send_message(message.chat.id, '-----------------------------------------------')
-                bot.send_message(message.chat.id, oCatolicoBotService)
-                bot.send_message(message.chat.id, '-----------------------------------------------')
-                bot.send_message(message.chat.id, botInfoRpi)  
+        old_ip = get_ip()
+        now = get_time()
+        bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
+        bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
+        bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
+        bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
+        bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
+            
+        oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service')
+        botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
+        bot.send_message(message.chat.id, '-----------------------------------------------')
+        bot.send_message(message.chat.id, oCatolicoBotService)
+        bot.send_message(message.chat.id, '-----------------------------------------------')
+        bot.send_message(message.chat.id, botInfoRpi)  
 
 @bot.message_handler(commands = ['reboot'])
 def send_reboot_command(message):
