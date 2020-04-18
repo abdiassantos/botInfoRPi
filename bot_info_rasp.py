ca64bab003382af9a15196b26bedfc0cc1916b70
abdiasantos <abdviana@gmail.com>
2020-04-18 11:02:12 -0300

    creating a requirements.txt fle to install dependencies

--- bot_info_rasp.py
+++ bot_info_rasp.py
@@ -6,9 +6,6 @@ from get_info import *
 token = '685098141:AAGEtCbrHfeixKRs5WreVTo6xYX4hvODqJg'
 bot = telebot.TeleBot(token)
 
-oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service')
-botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
-
 @bot.message_handler(commands = ['info', 'start'])
 def send_info(message):
         old_ip = get_ip()
@@ -16,19 +13,12 @@ def send_info(message):
         bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
         bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
         bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
-        bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
-        bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
-            
-        oCatolicoBotService = subprocess.getoutput('systemctl status botinforpi.service')
-        botInfoRpi = subprocess.getoutput('systemctl status ocatolicobot.service')
-        bot.send_message(message.chat.id, '-----------------------------------------------')
-        bot.send_message(message.chat.id, oCatolicoBotService)
-        bot.send_message(message.chat.id, '-----------------------------------------------')
-        bot.send_message(message.chat.id, botInfoRpi)  
+        # bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
+        # bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
 
-@bot.message_handler(commands = ['reboot'])
-def send_reboot_command(message):
-        bot.send_message(message.chat.id, 'Raspberry Reiniciado')
-        reboot_rasp()
+# @bot.message_handler(commands = ['reboot'])
+# def send_reboot_command(message):
+#         bot.send_message(message.chat.id, 'Raspberry Reiniciado')
+#         reboot_rasp()
 
 bot.polling()
