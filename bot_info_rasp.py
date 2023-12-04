cd21f571bbfa0f352c89ca11a0c41f0ee3b2dec5
Abdias Viana <abdviana@gmail.com>
2023-12-04 14:11:04 +0000

    Adjustment

--- bot_info_rasp.py
+++ bot_info_rasp.py
@@ -8,20 +8,20 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['info', 'start'])
 def send_info(message):
-        old_ip = get_ip()
+        #old_ip = get_ip()
         now = get_time()
         bot.send_message(message.chat.id, 'Hi Sir {}'.format(message.chat.username))
         bot.send_message(message.chat.id, 'System Time: {}/{}/{} - {}:{}:{}'.format(now.day, now.month, now.year, now.hour, now.minute, now.second))
         
-        if message.chat.id == 48443118:
-                bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
-        # bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
-        # bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
+        #if message.chat.id == 48443118:
+        #        bot.send_message(message.chat.id, 'IP Ext: {}'.format(str(old_ip)))
+        bot.send_message(message.chat.id, "CPU Temp: {:.1f}'C".format(get_cpu_temp()))
+        #bot.send_message(message.chat.id, 'GPU Temp: {}'.format(get_gpu_temp()))
 
 @bot.message_handler(commands = ['reboot'])
 def send_reboot_command(message):
         if message.chat.id == 48443118:
-                bot.send_message(message.chat.id, 'CASE Reiniciado')
+                bot.send_message(message.chat.id, 'TARS Reiniciado')
                 reboot_rasp()
         else:
                 bot.send.message(message.chat.id, 'Você não possui permissão para realizar esta ação')
